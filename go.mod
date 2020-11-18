27b6e0f6bc254ae4e4ce1c1f82cb9b84e25cd7b7
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-11-18 23:25:12 +0530

    fix: deserialize List Kind containing RawExtensions
    `Kind: List` (and others) may contain RawExtensions, which is made up of
    Raw JSON. These RawExtensions require a second pass through the
    deserializer to parse the manifests to go structs.
    
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.0
 	github.com/knadh/koanf v0.14.0
 	github.com/sirupsen/logrus v1.7.0
+	k8s.io/api v0.19.4
 	k8s.io/apimachinery v0.19.4
 	k8s.io/client-go v0.19.4
 )
--- pkg/kubeutils/kubeutils.go
+++ pkg/kubeutils/kubeutils.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	multierror "github.com/hashicorp/go-multierror"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -35,7 +36,27 @@ func Deserialize(m io.Reader) ([]runtime.Object, error) {
 		}
 
 		if obj != nil {
-			rto = append(rto, obj)
+			if obj.GetObjectKind().GroupVersionKind().Kind == "List" {
+				// Runtime Object is of `Kind: List`
+				// Lists may contain RawExtensions which are stored as
+				// JSON, and require additional pass through Deserializer
+				// Ref: https://kubernetes.io/docs/tasks/extend-kubernetes/custom-resources/custom-resource-definitions/#rawextension
+				// https://github.com/kubernetes/apimachinery/blob/master/pkg/runtime/types.go#L94
+				// Convert the RuntimeObject to a List Object before deserializing
+				listObj := obj.(*corev1.List)
+				for _, i := range listObj.Items {
+					// Deserialize the ListObject JSON
+					lo, err := deserialize(i.Raw)
+					if err != nil {
+						merror = multierror.Append(merror, err)
+					}
+					if lo != nil {
+						rto = append(rto, lo)
+					}
+				}
+			} else {
+				rto = append(rto, obj)
+			}
 		}
 	}
 	return rto, merror
